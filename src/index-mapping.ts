e934edd5870cf655c623ceef79ea648ca0bf2002
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2020-10-22 23:18:14 +0900

    Extract type definitions from index-mapping.ts

--- src/index-mapping.ts
+++ src/index-mapping.ts
@@ -1,71 +1,11 @@
 import { Client } from '@elastic/elasticsearch';
-
-type FieldDataType =
-  | FieldDataTypeCommon
-  | FieldDataTypeObject
-  | FieldDataTypeStructured
-  | FieldDataTypeAggregate
-  | FieldDataTypeTextSearch
-  | FieldDataTypeDocumentRanking
-  | FieldDataTypeSpatial
-  | undefined;
-
-type FieldDataTypeCommon =
-  | 'binary'
-  | 'boolean'
-  | FieldDataTypeKeyword
-  | FieldDataTypeNumber
-  | FieldDataTypeDate
-  | 'alias';
-
-type FieldDataTypeKeyword = 'keyword' | 'constant_keyword' | 'wildcard';
-
-type FieldDataTypeNumber = 'long' | 'integer' | 'short' | 'byte' | 'double' | 'float' | 'half_float' | 'scaled_float';
-
-type FieldDataTypeDate = 'date' | 'date_nanos';
-
-type FieldDataTypeObject = 'object' | 'flattened' | 'nested' | 'join';
-
-type FieldDataTypeStructured = FieldDataTypeRange | 'ip' | 'murmur';
-
-type FieldDataTypeRange = 'integer_range' | 'float_range' | 'long_range' | 'double_range' | 'date_range' | 'ip_range';
-
-type FieldDataTypeAggregate = 'histogram';
-
-type FieldDataTypeTextSearch = 'text' | 'annotated-text' | 'completion' | 'search_as_you_type' | 'token_count';
-
-type FieldDataTypeDocumentRanking = 'dense_vector' | 'rank_feature' | 'rank_features';
-
-type FieldDataTypeSpatial = 'geo_point' | 'geo_shape' | 'point' | 'shape';
-
-interface FieldDefinition {
-  type: FieldDataType;
-}
-
-interface FieldDefinitionNested extends FieldDefinition {
-  type: 'nested';
-  properties:
-    | undefined
-    | {
-        [fieldName: string]: FieldDefinition | FieldDefinitionNested;
-      };
-}
-
-interface IndicesStatsResponse {
-  indices: {
-    [indexName: string]: Record<string, unknown>;
-  };
-}
-
-interface IndicesGetMappingResponse {
-  [indexName: string]: {
-    mappings: {
-      properties: {
-        [fieldName: string]: FieldDefinition | FieldDefinitionNested;
-      };
-    };
-  };
-}
+import {
+  FieldDataType,
+  FieldDefinition,
+  FieldDefinitionNested,
+  IndicesGetMappingResponse,
+  IndicesStatsResponse
+} from "./es/indices";
 
 export class IndexMappingField {
   readonly name: string;
