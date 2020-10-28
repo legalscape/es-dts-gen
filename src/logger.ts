0f8d2380be3cd2aa766a9e47583a094a381f279e
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2020-10-28 15:17:06 +0900

    Configure logger to output stack trace

--- src/logger.ts
+++ src/logger.ts
@@ -3,9 +3,14 @@ import * as winston from 'winston';
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
+    winston.format.errors({ stack: true }),
     winston.format.timestamp(),
     winston.format.cli(),
-    winston.format.printf((info) => `[${info.timestamp}] ${info.level} ${info.message}`)
+    winston.format.printf((info) =>
+      info.stack
+        ? `${info.timestamp}  ${info.level} ${info.message} ${info.stack}`
+        : `${info.timestamp}  ${info.level} ${info.message}`
+    )
   ),
   transports: [new winston.transports.Console()],
 });
